5018e880398cc8209beded3112fad74797198824
InukaSilv <159909808+InukaSilv@users.noreply.github.com>
2024-02-13 22:53:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 $(document).ready(function() {
     $('#heart12').click(function() {
-        $('#heart1').css({
-            animationName: 'fadeOut',
-            animationDuration: '0.5s'
-        });
+        $('#heart12').fadeOut('slow', function() {
+            setTimeout(function() {
+                window.location.href = 'letter.html';
+            }, 500);
+        })
 
-        setTimeout(function() {
-            window.location.href = 'letter.html';
-        }, 1000);
-        // Load letter.html content
         
+
         
     });
 });
-
 
